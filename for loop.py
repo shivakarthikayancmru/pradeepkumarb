fdd80d4ec83c9864161db7ce9cf57767e2d41835
shivakarthikayancmru <35587775+shivakarthikayancmru@users.noreply.github.com>
2018-02-23 10:26:53 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,6 @@
+for i in range(10):
+    print(i)
+for i in range(2,30,3):
+    print(i)
+
+    
---
+++ while loop program.py
@@ -0,0 +1,25 @@
+for i in[1,2,3,4,5]:
+    if(i==3):
+        break
+    print(i)
+for i in[1,2,3,4,5]:
+    if(i==3):
+        continue
+    print(i)
+for i in [1,2,3,4,5,6,7]:
+    if(i==5):
+        break
+    print(i)
+
+print("out of for loop")
+print(i)
+
+for i in [1,2,3,4,5,6,7]:
+    if(i==1):
+        break
+    print(i)
+
+print("out of for loop")
+print(i)
+
+
