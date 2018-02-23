32b2fe3b476c3abb704bbea601f034f9b14c868e
shivakarthikayancmru <35587775+shivakarthikayancmru@users.noreply.github.com>
2018-02-23 10:25:26 +0530

    Add files via upload

---
+++ add sub mul div.py
@@ -0,0 +1,22 @@
+def add(a,b):
+    print(a+b)
+    print("addition success")
+def sub(x,y):
+    print("success")
+    return x-y
+
+add(200,300)
+g=sub(300,200)
+
+
+
+def mul(a,b):
+    print("jj")
+    print(a*b)
+def div(x,y):
+    print("hh")
+    return x/y
+
+mul(20,30)
+d=div(40,50)
+print(d)
